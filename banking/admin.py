53ae1f547bead55c26fab2ea5ed99d82b979baa4
Zita Nayra Ardini <zita.nayra@ui.ac.id>
2026-05-01 12:26:48 +0700

    Create banking model and migration.

--- banking/admin.py
+++ banking/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Rekening, Transaksi, TopUp, Notifikasi
 
-# Register your models here.
+
+@admin.register(Rekening)
+class RekeningAdmin(admin.ModelAdmin):
+    list_display = ['nomor_rekening', 'pemilik', 'saldo', 'aktif', 'dibuat_pada']
+    list_filter = ['aktif']
+    search_fields = ['nomor_rekening', 'pemilik__username']
+
+
+@admin.register(Transaksi)
+class TransaksiAdmin(admin.ModelAdmin):
+    list_display = ['id', 'jenis', 'nominal', 'rekening_asal', 'rekening_tujuan', 'status', 'waktu']
+    list_filter = ['jenis', 'status']
+    search_fields = ['rekening_asal__nomor_rekening', 'rekening_tujuan__nomor_rekening']
+
+
+@admin.register(TopUp)
+class TopUpAdmin(admin.ModelAdmin):
+    list_display = ['id', 'rekening', 'nominal', 'metode', 'status', 'diproses_oleh', 'waktu_request']
+    list_filter = ['status', 'metode']
+
+
+@admin.register(Notifikasi)
+class NotifikasiAdmin(admin.ModelAdmin):
+    list_display = ['user', 'pesan', 'dibaca', 'waktu']
+    list_filter = ['dibaca']
