bb9284bface78af2934d17561e5f3008f94c0656
namarabigail <bigelnp10@gmail.com>
2026-05-24 10:10:27 +0700

    test: add Broken Authentication unit tests and cache mitigation for banking

--- banking/tests.py
+++ banking/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from banking.models import Rekening
 
-# Create your tests here.
+class BankingAuthTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(
+            username='nasabah_uji', password='Password123!', role='nasabah'
+        )
+        self.rekening = Rekening.objects.create(
+            pemilik=self.user, nomor_rekening='1234567890', saldo=500000, aktif=True
+        )
+
+    def test_akses_halaman_transfer_tanpa_login(self):
+        """Uji apakah guest bisa akses transfer (Harus Gagal/Redirect)"""
+        response = self.client.get(reverse('banking:transfer'))
+        self.assertEqual(response.status_code, 302) # Harus redirect ke login
+
+    def test_proteksi_cache_halaman_mutasi(self):
+        """Uji @never_cache pada mutasi (Mitigasi Back-Button Attack)"""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('banking:mutasi'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('no-cache', response.get('Cache-Control', ''))
+        self.assertIn('no-store', response.get('Cache-Control', ''))
